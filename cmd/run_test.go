11de1c347941c48ad01a0e40f87596559789e296
Garrett Massman <gmassman20@gmail.com>
2019-12-04 13:25:45 -0800

    updated test

--- cmd/run_test.go
+++ cmd/run_test.go
@@ -1,9 +1,7 @@
 package cmd
 
 import (
-	"bytes"
 	"fmt"
-	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,20 +10,20 @@ import (
 func TestFactorial(t *testing.T) {
 	tt := []struct {
 		input  int
-		output string
+		output int
 	}{
-		{input: 0, output: "result: 1"},
-		{input: 1, output: "result: 1"},
-		{input: 2, output: "result: 2"},
-		{input: 5, output: "result: 120"},
-		{input: 10, output: "result: 3628800"},
+		{input: 0, output: 1},
+		{input: 1, output: 1},
+		{input: 2, output: 2},
+		{input: 5, output: 120},
+		{input: 10, output: 3628800},
 	}
 
 	for _, tc := range tt {
 		t.Run(fmt.Sprintln("factorial of", tc.input), func(t *testing.T) {
-			var buf bytes.Buffer
-			factorial(tc.input, func(i int) { buf.WriteString("result: " + strconv.Itoa(i)) })
-			assert.Equal(t, tc.output, buf.String())
+			factorial(tc.input, func(i int) {
+				assert.Equal(t, tc.output, i)
+			})
 		})
 	}
 }
